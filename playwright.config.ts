d6c5d333447ec1955e3797037e422b03b1a0fd7c
Rahul Suthar <rahul.suthar@apexon.com>
2026-09-24 12:08:00 +0530

    allure reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'], 
-    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+    ['html'],
+    ['allure-playwright', { outputFolder: 'allure-results' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
